62c4c2ab854f9cb533ee31e250e71d4a8d9b53f8
Dan Crews <dancrews@me.com>
2018-05-18 16:50:35 -0400

    Pull out callbacks for admin pages.

--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -4,8 +4,9 @@
  */
 namespace Inc\Pages;
 
-use \Inc\Base\BaseController;
-use \Inc\Api\SettingsApi;
+use Inc\Api\SettingsApi;
+use Inc\Base\BaseController;
+use Inc\Api\Callbacks\AdminCallbacks;
 
 /**
 *
@@ -15,54 +16,67 @@ class Admin extends BaseController
 
 	public $settings;
 
+	public $callbacks;
+
 	public $pages = [];
 
-	public function __construct()
+	public $subpages = [];
+
+	public function register()
 	{
 		$this->settings = new SettingsApi();
 
-		$this->pages = [
-			[
-				'page_title' => 'Ooopy Plugin',
+		$this->callbacks = new AdminCallbacks();
+
+		$this->setPages();
+
+		$this->setSubpages();
+
+		$this->settings->addPages( $this->pages )->withSubPage( 'Dashboard' )->addSubPages( $this->subpages )->register();
+	}
+
+	public function setPages()
+	{
+		$this->pages = array(
+			array(
+				'page_title' => 'Oopy Plugin',
 				'menu_title' => 'Oopy',
 				'capability' => 'manage_options',
 				'menu_slug' => 'oopy_plugin',
-				'callback' => function() { echo '<h1>Oopy Plugin</h1>'; },
+				'callback' => array( $this->callbacks, 'adminDashboard' ),
 				'icon_url' => 'dashicons-store',
 				'position' => 110
-			]
-		];
+			)
+		);
+	}
 
-		$this->subpages = [
-			[
+	public function setSubpages()
+	{
+		$this->subpages = array(
+			array(
 				'parent_slug' => 'oopy_plugin',
 				'page_title' => 'Custom Post Types',
 				'menu_title' => 'CPT',
 				'capability' => 'manage_options',
 				'menu_slug' => 'oopy_cpt',
-				'callback' => function() { echo '<h1>CPT Manager</h1>'; }
-			],
-			[
+				'callback' => array( $this->callbacks, 'adminCpt' )
+			),
+			array(
 				'parent_slug' => 'oopy_plugin',
 				'page_title' => 'Custom Taxonomies',
 				'menu_title' => 'Taxonomies',
 				'capability' => 'manage_options',
 				'menu_slug' => 'oopy_taxonomies',
-				'callback' => function() { echo '<h1>Taxonomies Manager</h1>'; }
-			],
-			[
+				'callback' => array( $this->callbacks, 'adminTaxonomy' )
+			),
+			array(
 				'parent_slug' => 'oopy_plugin',
 				'page_title' => 'Custom Widgets',
 				'menu_title' => 'Widgets',
 				'capability' => 'manage_options',
 				'menu_slug' => 'oopy_widgets',
-				'callback' => function() { echo '<h1>Widgets Manager</h1>'; }
-			]
-		];
-	}
-
-	public function register()
-	{
-		$this->settings->addPages( $this->pages )->withSubPage( 'Dashboard' )->addSubPages( $this->subpages )->register();
+				'callback' => array( $this->callbacks, 'adminWidget' )
+			)
+		);
 	}
 }
