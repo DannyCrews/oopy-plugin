156378412747e525cafd43ab5806ad2fc6e9ed65
Dan Crews <dancrews@me.com>
2018-05-18 16:06:00 -0400

    add admin menu subpages.

--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -32,10 +32,37 @@ class Admin extends BaseController
 				'position' => 110
 			]
 		];
+
+		$this->subpages = [
+			[
+				'parent_slug' => 'oopy_plugin',
+				'page_title' => 'Custom Post Types',
+				'menu_title' => 'CPT',
+				'capability' => 'manage_options',
+				'menu_slug' => 'oopy_cpt',
+				'callback' => function() { echo '<h1>CPT Manager</h1>'; }
+			],
+			[
+				'parent_slug' => 'oopy_plugin',
+				'page_title' => 'Custom Taxonomies',
+				'menu_title' => 'Taxonomies',
+				'capability' => 'manage_options',
+				'menu_slug' => 'oopy_taxonomies',
+				'callback' => function() { echo '<h1>Taxonomies Manager</h1>'; }
+			],
+			[
+				'parent_slug' => 'oopy_plugin',
+				'page_title' => 'Custom Widgets',
+				'menu_title' => 'Widgets',
+				'capability' => 'manage_options',
+				'menu_slug' => 'oopy_widgets',
+				'callback' => function() { echo '<h1>Widgets Manager</h1>'; }
+			]
+		];
 	}
 
 	public function register()
 	{
-		$this->settings->addPages( $this->pages )->register();
+		$this->settings->addPages( $this->pages )->withSubPage( 'Dashboard' )->addSubPages( $this->subpages )->register();
 	}
 }
